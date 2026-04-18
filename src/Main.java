4c3dd2779ca62b28d51df4480499a503bb56c72c
Vusal Abbasov <vusalabbasovredmi9@gmail.com>
2026-04-18 19:38:53 +0400

    implement basic console calculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("--- Basic Calculator ---");
+
+        System.out.print("Add the first number: ");
+        double num1 = new Scanner(System.in).nextDouble();
+
+        System.out.print("Select the operation (+, -, *, /): ");
+        String operation = new Scanner(System.in).nextLine();
+
+        System.out.print("Add the second number: ");
+        double num2 = new Scanner(System.in).nextDouble();
+
+        switch (operation) {
+            case "+":
+                System.out.println("Result: " + num1 + " + " + num2 + " = " + (num1 + num2));
+                break;
+            case "-":
+                System.out.println("Result: " + num1 + " - " + num2 + " = " + (num1 - num2));
+                break;
+            case "*":
+                System.out.println("Result: " + num1 + " * " + num2 + " = " + (num1 * num2));
+                break;
+            case "/":
+                System.out.println("Result: " + num1 + " / " + num2 + " = " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid operation!");
+        }
 
     }
 }
